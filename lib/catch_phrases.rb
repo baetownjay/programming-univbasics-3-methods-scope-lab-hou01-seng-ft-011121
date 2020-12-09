06c486146490b13dc5c96174e63a668f36bff8ab
Jason Herrera <baytownjason@yahoo.com>
2020-12-09 21:13:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,11 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  link_saying = "It's Dangerous To Go Alone! Take This."
+  puts link_saying
+end
+
+def all_phrases
+  combo = 
